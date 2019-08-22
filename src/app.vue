d913a25c2eded2bad31d2da00fb2e834989af57c
canfoo <178774618@qq.com>
2019-08-22 21:15:20 +0800

    计数器

--- src/app.vue
+++ src/app.vue
@@ -1,13 +1,35 @@
 <template>
-    <div id="app">{{text}}</div>
+    <div>
+        <div id="app">{{text}}</div>
+        <counter></counter>
+    </div>
 </template>
+
 <script>
+    import counter from './counter.vue'
     export default {
         data() {
             return {
                 text: '236' 
             }
+        },
+        components: {
+            counter
+        },
+        methods: {
+            test() {
+                console.log('test')
+            }
+        },
+        mounted() {
+            console.log(123)
+            this.test()
+            this.text = 'hello'
         }
     }
 </script>
-<style></style>
+<style>
+#app {
+    color: red;
+}
+</style>
---
+++ src/counter.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        计数器
+    </div>
+</template>
+<script>
+    export default {
+    }
+</script>
+<style>
+
+</style>
