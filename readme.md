5ffe859bd7fde46e5d25aafd6288ff6837ed13e4
ublooth <695927047@qq.com>
2019-08-25 21:38:40 +0800

    no1

---
+++ readme.md
@@ -0,0 +1,16 @@
+#vue-webpack-demo
+
+#安装
+```
+npm install
+```
+
+#运行
+```
+npm run dev
+```
+
+#构建(打包，生成html，JS)
+```
+npm run build
+```
--- src/app.vue
+++ src/app.vue
@@ -1,13 +1,40 @@
 <template>
-    <div id="app">{{text}}</div>
+    <div>
+        <!-- <div id="app">{{text}}</div>
+        <counter @message2="asd"></counter> -->
+        <div id="app">
+            <h1>My Todo App123!</h1>
+            <TodoList/>
+        </div>
+    </div>
+    
 </template>
+
 <script>
+// import counter from './counter.vue'
+// import { setTimeout } from 'timers';
+import TodoList from './components/TodoList.vue'
     export default {
         data() {
             return {
-                text: '236' 
+                text: 'test vue' 
+            }
+        },
+        components:{
+            // counter
+            TodoList
+        },
+        methods: {
+            getMeaasge() {
+                
+                console.log(123)
+            },
+            asd() {
+                // window.clearTimeout();
+                console.log(num)
             }
         }
     }
+    
 </script>
 <style></style>
